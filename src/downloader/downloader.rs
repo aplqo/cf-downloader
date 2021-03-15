3c15a4ce7d8b6a20f386428c690ff92409ebc9cb
aplqo <aplqo@outlook.com>
2021-03-15 17:58:53 +0800

    [downloader] Finished dowloader

--- src/downloader/downloader.rs
+++ src/downloader/downloader.rs
@@ -1,53 +1,98 @@
 extern crate serde;
 
 use crate::{
-    client::{Problem, Session},
-    encoding::traits::{DataDecoder, DataEncoder, MetaEncoding},
-    types::{Error, Template, TestMeta, BLOCK},
+    client::{Problem, Session, Verdict},
+    encoding::{
+        traits::{DataDecoder, DataEncoder, MetaEncoding},
+        Template,
+    },
+    types::{Result, TestMeta, BLOCK},
 };
 use futures::executor::block_on;
-use std::{borrow::Borrow, thread::sleep, time::Duration, vec::Vec};
+use std::{fs::File, path, vec::Vec};
 
 pub struct Downloader<'a> {
     session: &'a Session,
-    delay: Duration,
     problem: &'a Problem,
-    testdata: Vec<TestMeta>,
+    pub testdata: Vec<TestMeta>,
 }
 
 impl<'a> Downloader<'a> {
-    pub fn new(session: &'a Session, delay: Duration, problem: &'a Problem) -> Self {
+    pub fn new(session: &'a Session, problem: &'a Problem) -> Self {
         Downloader {
             session,
-            delay,
             problem,
             testdata: Vec::new(),
         }
     }
-    pub fn get_meta<Enc, T>(
-        &mut self,
-        template: &Template,
-        count: usize,
-        call: T,
-    ) -> Result<(), Error>
+    async fn submit_code(&self, lang: &'a str, code: &'a str) -> Result<Verdict> {
+        self.session.submit(&self.problem, lang, code).await?;
+        let sub = self.session.get_last_submission(&self.problem).await?;
+        while !sub.is_judged(self.session).await {}
+        sub.get_verdict(self.session).await
+    }
+
+    pub fn get_meta<'b, Enc>(&mut self, template: &Template, count: usize) -> Result<()>
     where
-        Enc: MetaEncoding,
-        T: Fn(usize) -> (),
+        Enc: MetaEncoding<'b>,
     {
         self.testdata.reserve(count);
-        let mut enc = Enc::new(template, count);
+        let base = self.testdata.len();
+        let mut enc = Enc::new(template, count + base)?;
+        unsafe {
+            for i in 0..base {
+                enc.ignore(&(*self.testdata.as_ptr().add(i)).data_id);
+            }
+        }
         for i in 0..count - 1 {
-            call(i);
             self.testdata.push(Enc::decode(block_on(async {
-                self.session
-                    .submit(self.problem, template.language, enc.generate_encoder())
-                    .await?;
-                let url = self.session.get_last_submission_url(self.problem).await?;
-                self.session.wait_submission(&url).await
+                self.submit_code(template.language, enc.generate()?).await
             })?)?);
-            sleep(self.delay);
-            enc.ignore_input(&self.testdata[i].hash);
+            unsafe {
+                enc.ignore(&(*self.testdata.as_ptr().add(base + i)).data_id);
+            }
         }
         Ok(())
+    }
+    pub fn load_meta(&mut self, dest: &path::Path) -> Result<()> {
+        Ok(self.testdata = serde_yaml::from_reader(File::open(dest)?)?)
+    }
+    pub fn save_meta(&self, dest: &path::Path) -> Result<()> {
+        Ok(serde_yaml::to_writer(File::create(dest)?, &self.testdata)?)
+    }
+
+    pub async fn get_data<'b, Enc, Dec>(
+        &'b self,
+        template: &Template<'b>,
+        begin: usize,
+        end: usize,
+    ) -> Result<Vec<String>>
+    where
+        Enc: DataEncoder<'b>,
+        Dec: DataDecoder,
+    {
+        let mut encoder = Enc::new(template, end)?;
+        let mut decoder = Dec::new();
+        for i in &self.testdata[0..begin] {
+            encoder.push_ignore(&i.data_id);
+        }
+        let mut ret: Vec<String> = Vec::new();
+        ret.reserve(end - begin);
+        for i in &self.testdata[begin..end] {
+            decoder.init(i);
+            let count = (i.output_size + BLOCK - 1) / BLOCK;
+            for j in 0..count {
+                decoder.add_message(
+                    j * BLOCK,
+                    &self
+                        .submit_code(template.language, encoder.generate(j * BLOCK)?)
+                        .await?
+                        .output,
+                );
+            }
+            ret.push(decoder.decode()?);
+            encoder.push_ignore(&i.data_id);
+        }
+        Ok(ret)
     }
 }
--- src/downloader/types.rs
+++ src/downloader/types.rs
@@ -1,17 +1,36 @@
 extern crate serde;
+
 use serde::{Deserialize, Serialize};
+
 #[derive(Serialize, Deserialize)]
-pub struct TestMeta {
+pub struct DataId {
     pub hash: String,
-    pub size: usize,
-    pub output_size: usize,
     pub(crate) answer: Option<String>,
 }
-pub struct Template<'a> {
-    pub language: &'a String,
-    pub content: &'a String,
+#[derive(Serialize, Deserialize)]
+pub struct TestMeta {
+    pub data_id: DataId,
+    pub size: usize,
+    pub output_size: usize,
+    pub compress_size: usize,
 }
-pub struct Error {
-    pub description: String,
-}
+
 pub const BLOCK: usize = 500;
+
+#[derive(Debug)]
+pub struct Error {
+    description: String,
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::write!(f, "{}", self.description)
+    }
+}
+impl std::error::Error for Error {}
+impl Error {
+    pub fn new(description: String) -> Box<Self> {
+        Box::new(Error { description })
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
