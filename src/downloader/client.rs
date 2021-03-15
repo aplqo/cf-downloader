10d9943c538b270275f2bc9929ed92894b0c6b5d
aplqo <aplqo@outlook.com>
2021-03-15 19:16:13 +0800

    [client] Finished API client

--- src/downloader/client.rs
+++ src/downloader/client.rs
@@ -1,10 +1,11 @@
 extern crate regex;
 extern crate reqwest;
 
-use crate::types::Error;
+use crate::types::{Error, Result};
 use futures::executor::block_on;
 use regex::Regex;
 use reqwest::{Client, Proxy};
+use std::{collections::HashMap, result::Result as StdResult};
 
 const BFAA: &str = "f1b3f18c715565b589b7823cda7448ce";
 const FIREFOX_UA: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0";
@@ -16,62 +17,93 @@ pub enum ProblemType {
 pub struct Problem {
     pub source: ProblemType,
     pub contest: String,
-    id: String,
+    pub id: String,
 }
-pub struct Submission {
+pub struct Verdict {
     pub output: String,
     pub answer: Option<String>,
 }
+
 struct UtilityRegex {
     csrf: Regex,
     login: Regex,
     submit: Regex,
+    last_submit: Regex,
 }
-pub struct Session {
-    client: Client,
-    handle: String,
-    ftaa: String,
-    regex: UtilityRegex,
-}
-
-impl std::convert::From<reqwest::Error> for Error {
-    fn from(e: reqwest::Error) -> Error {
-        Error {
-            description: e.to_string(),
-        }
-    }
-}
-
 impl UtilityRegex {
     fn new() -> Self {
         UtilityRegex {
             csrf: Regex::new("csrf='(.+?)'").unwrap(),
             login: Regex::new(r#"handle = "([\s\S]+?)"#).unwrap(),
             submit: Regex::new(r#"error[a-zA-Z_\-\ ]*">(.*?)</span>"#).unwrap(),
+            last_submit: Regex::new(r#"<tr class="last-row" data-submission-id="([[:digit:]]+)""#)
+                .unwrap(),
         }
     }
 }
 
+pub struct Submission {
+    id: String,
+    csrf_token: String,
+}
+impl Submission {
+    async fn get_info(
+        &self,
+        session: &Session,
+    ) -> StdResult<HashMap<String, String>, reqwest::Error> {
+        session
+            .client
+            .get("https://codeforces.com/data/submitSource")
+            .query(&[("submissionId", &self.id), ("csrf_token", &self.csrf_token)])
+            .send()
+            .await?
+            .json::<HashMap<String, String>>()
+            .await
+    }
+    pub async fn is_judged(&self, session: &Session) -> bool {
+        self.get_info(session).await.map_or(false, |v| {
+            let p = &v["verdict"];
+            !p.contains("verdict-waiting")
+        })
+    }
+    pub async fn get_verdict(&self, session: &Session) -> Result<Verdict> {
+        let mut data = self.get_info(session).await?;
+        let pos = data.remove("testCount").unwrap();
+        let output = data.remove(&format!("output#{}", pos)).unwrap();
+        let answer = data.remove(&format!("answer#{}", pos)).unwrap();
+        Ok(Verdict {
+            output,
+            answer: if answer.len() > 500 {
+                None
+            } else {
+                Some(answer)
+            },
+        })
+    }
+}
+
+pub struct Session {
+    client: Client,
+    handle: String,
+    ftaa: String,
+    regex: UtilityRegex,
+}
 impl Session {
-    pub fn new(handle: String, proxy: &str) -> Result<Self, String> {
-        let proxy = match Proxy::https(proxy) {
-            Ok(p) => p,
-            Err(e) => return Err(e.to_string()),
-        };
+    pub fn new(handle: String, proxy: &str) -> Result<Self> {
         Ok(Session {
             client: Client::builder()
-                .proxy(proxy)
+                .proxy(Proxy::https(proxy)?)
                 .user_agent(FIREFOX_UA)
                 .cookie_store(true)
                 .build()
                 .unwrap(),
-            handle: handle,
+            handle,
             ftaa: "123456789123456789".to_string(),
             regex: UtilityRegex::new(),
         })
     }
 
-    async fn get_csrf(&self, url: &str) -> Result<String, reqwest::Error> {
+    async fn get_csrf(&self, url: &str) -> StdResult<String, reqwest::Error> {
         let body = self.client.get(url).send().await?.text().await?;
         Ok(self
             .regex
@@ -84,7 +116,7 @@ impl Session {
             .to_string())
     }
 
-    pub fn login(&self, password: &str) -> Result<(), Error> {
+    pub fn login(&self, password: &str) -> Result<()> {
         const URL: &str = "https://codeforces.com/enter";
         let body = block_on(async {
             let csrf = self.get_csrf(URL).await?;
@@ -108,20 +140,39 @@ impl Session {
         if self.regex.login.is_match(body.as_str()) {
             Ok(())
         } else {
-            Err(Error {
-                description: "Failed to log into codeforces.com".to_string(),
-            })
+            Err(Error::new("Failed to log into codeforces.com".to_string()))
         }
     }
-    pub async fn get_last_submission_url(&self, problem: &Problem) -> Result<String, Error> {
-        Ok(String::new())
+    pub async fn get_last_submission(&self, problem: &Problem) -> Result<Submission> {
+        let url = format!("https://codeforces.com/contest/{}/status", problem.contest);
+        let csrf = self.get_csrf(url.as_str()).await?;
+        let body = self
+            .client
+            .post(url)
+            .query(&[
+                ("csrf_token", csrf.as_str()),
+                ("action", "setupSubmissionFilter"),
+                ("frameProblemIndex", problem.id.as_str()),
+                ("verdictName", "anyVerdict"),
+                ("programTypeForInvoker", "anyProgramTypeForInvoker"),
+                ("comparisonType", "NOT_USED"),
+                ("judgedTestCount", ""),
+                ("participantSubstring", self.handle.as_str()),
+                ("_tta", "373"),
+            ])
+            .send()
+            .await?
+            .text()
+            .await?;
+        match self.regex.last_submit.captures(body.as_str()) {
+            Some(id) => Ok(Submission {
+                id: id.get(1).unwrap().as_str().to_string(),
+                csrf_token: csrf,
+            }),
+            None => Err(Error::new("Can't find last submission url".to_string())),
+        }
     }
-    pub async fn wait_submission(&self, url: &String) -> Result<Submission, Error> {
-        Err(Error {
-            description: String::new(),
-        })
-    }
-    pub async fn submit(&self, problem: &Problem, language: &str, code: &str) -> Result<(), Error> {
+    pub async fn submit(&self, problem: &Problem, language: &str, code: &str) -> Result<()> {
         let csrf = self.get_csrf("https://codeforces.com").await?;
         let body = self
             .client
@@ -147,22 +198,18 @@ impl Session {
             .text()
             .await?;
         match self.regex.submit.captures(body.as_str()) {
-            Some(err) => Err(Error {
-                description: err.get(1).unwrap().as_str().to_string(),
-            }),
+            Some(err) => Err(Error::new(err.get(1).unwrap().as_str().to_string())),
             None => Ok(()),
         }
     }
 
-    pub fn logout(&self) -> Result<(), String> {
-        match block_on(async {
+    pub fn logout(&self) -> Result<()> {
+        block_on(async {
             self.client
                 .get("https://codeforces.com/logout")
                 .send()
-                .await
-        }) {
-            Ok(_) => Ok(()),
-            Err(e) => return Err(e.to_string()),
-        }
+                .await?;
+            Ok(())
+        })
     }
 }
