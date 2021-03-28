73cce2a521214ac8eae0e3ccc8d62fbfef0efc34
aplqo <aplqo@outlook.com>
2021-03-28 20:18:15 +0800

    [random] Add random hex string generate

--- src/downloader/random.rs
+++ src/downloader/random.rs
@@ -1,22 +1,21 @@
 extern crate rand;
 
-use rand::{
-    distributions::{Alphanumeric, Standard},
-    prelude::Distribution,
-    thread_rng, Rng,
-};
-use std::iter;
+use rand::{distributions::Standard, prelude::Distribution, thread_rng, Rng};
 
-pub fn random_string(length: usize) -> String {
-    iter::repeat(())
-        .map(|()| thread_rng().sample(Alphanumeric))
-        .map(char::from)
-        .take(length)
-        .collect()
-}
 pub fn random_standard<T>() -> T
 where
     Standard: Distribution<T>,
 {
-    rand::thread_rng().sample(Standard)
+    thread_rng().sample(Standard)
+}
+pub fn random_hex(length: usize) -> String {
+    let mut ret = thread_rng()
+        .sample_iter::<u64, _>(Standard)
+        .take(length + 15 / 16)
+        .fold(String::new(), |mut acc, v| {
+            acc.push_str(v.to_string().as_str());
+            acc
+        });
+    ret.truncate(length);
+    ret
 }
