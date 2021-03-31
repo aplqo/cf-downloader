f73b86a95b8d6e8ecf8ca789f6ccf8e9d32ed6cb
aplqo <aplqo@outlook.com>
2021-03-31 22:28:58 +0800

    [account] Fix some mistakes

--- src/downloader/account.rs
+++ src/downloader/account.rs
@@ -73,6 +73,7 @@ pub async fn register(
             Ok(_) => {
                 vec_ses.push(client);
                 vec_acc.push(cur);
+                client = Session::new();
             }
             Err(e) => {
                 return (Some((vec_acc, vec_ses)), Some(RegisterError::Judge(e)));
