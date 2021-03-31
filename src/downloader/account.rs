b10acc0d837cb6f0a1ceca1bf0f86792f4cc2b18
aplqo <aplqo@outlook.com>
2021-03-31 08:23:06 +0800

    [account] Update error handling

--- src/downloader/account.rs
+++ src/downloader/account.rs
@@ -4,18 +4,40 @@ extern crate tokio;
 
 use crate::{
     config::register::{HANDLE_LEN, PASSWORD_LEN, REGISTER_DELAY},
-    email::Email,
-    judge::session::Session,
+    email::{self, Email},
+    judge::{self, session::Session},
     random::random_hex,
-    types::Result,
 };
 use serde::{Deserialize, Serialize};
 use std::{
     error::Error,
+    fmt,
     io::{Read, Write},
     vec::Vec,
 };
 use tokio::time::sleep;
+
+#[derive(Debug)]
+pub enum RegisterError {
+    Email(email::Error),
+    Judge(judge::error::Error),
+}
+impl fmt::Display for RegisterError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RegisterError::Email(err) => write!(f, "Error getting email address: {}", err),
+            RegisterError::Judge(err) => write!(f, "Error registering: {}", err),
+        }
+    }
+}
+impl Error for RegisterError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            RegisterError::Email(err) => Some(err),
+            RegisterError::Judge(err) => Some(err),
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Account {
@@ -24,22 +46,22 @@ pub struct Account {
     pub proxy: Option<String>,
 }
 
-pub fn from_reader<R: Read>(rdr: R) -> Result<Vec<Account>> {
-    Ok(serde_yaml::from_reader(rdr)?)
+pub fn from_reader<R: Read>(rdr: R) -> Result<Vec<Account>, serde_yaml::Error> {
+    serde_yaml::from_reader(rdr)
 }
 pub async fn register(
     count: usize,
-) -> (Option<(Vec<Account>, Vec<Session>)>, Option<Box<dyn Error>>) {
+) -> (Option<(Vec<Account>, Vec<Session>)>, Option<RegisterError>) {
     let mut email = Email::new();
     if let Err(e) = email.init().await {
-        return (None, Some(e));
+        return (None, Some(RegisterError::Email(e)));
     }
     let mut vec_acc = Vec::with_capacity(count);
     let mut vec_ses = Vec::with_capacity(count);
     let mut client = Session::new();
     for _ in 0..count {
         if let Err(e) = email.new_address().await {
-            return (Some((vec_acc, vec_ses)), Some(e));
+            return (Some((vec_acc, vec_ses)), Some(RegisterError::Email(e)));
         }
         let cur = Account {
             handle: random_hex(HANDLE_LEN),
@@ -54,14 +76,13 @@ pub async fn register(
                 client = Session::new();
             }
             Err(e) => {
-                return (Some((vec_acc, vec_ses)), Some(e));
+                return (Some((vec_acc, vec_ses)), Some(RegisterError::Judge(e)));
             }
         }
         sleep(REGISTER_DELAY).await;
     }
     (Some((vec_acc, vec_ses)), None)
 }
-pub fn to_writer<W: Write>(wdr: W, list: &[Account]) -> Result<()> {
-    serde_yaml::to_writer(wdr, list)?;
-    Ok(())
+pub fn to_writer<W: Write>(wdr: W, list: &[Account]) -> Result<(), serde_yaml::Error> {
+    serde_yaml::to_writer(wdr, list)
 }
