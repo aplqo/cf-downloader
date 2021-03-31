e44b0892dc1e2a015684b9a7592197c5989ecddb
aplqo <aplqo@outlook.com>
2021-03-31 15:51:04 +0800

    [downloader] Remove unused types in types.rs

--- src/downloader/types.rs
+++ src/downloader/types.rs
@@ -17,21 +17,3 @@ pub struct TestMeta {
 }
 
 pub const BLOCK: usize = 500;
-
-#[derive(Debug)]
-pub struct Error {
-    description: String,
-}
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::write!(f, "{}", self.description)
-    }
-}
-impl std::error::Error for Error {}
-impl Error {
-    pub fn new(description: String) -> Box<Self> {
-        Box::new(Error { description })
-    }
-}
-
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
