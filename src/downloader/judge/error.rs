698b31c331ee03ea5724cfbb79662438296ce70d
aplqo <aplqo@outlook.com>
2021-03-31 08:07:32 +0800

    [judge] Remove boxing in error handling

--- src/downloader/judge/error.rs
+++ src/downloader/judge/error.rs
@@ -4,18 +4,16 @@ use crate::email;
 use std::{boxed::Box, convert::Into, error::Error as StdError, fmt, result::Result as StdResult};
 
 #[derive(Debug)]
-pub(crate) struct Error(Box<Inner>);
-#[derive(Debug)]
 pub(super) enum Kind {
     Builder(reqwest::Error),
     Network(reqwest::Error),
-    CSRF(Box<Inner>),
+    CSRF(Box<Error>),
     API,
     Regex,
     Email(email::Error),
     TestCount(usize, usize),
 }
-struct Inner {
+pub struct Error {
     kind: Kind,
     description: Option<String>,
 }
@@ -24,11 +22,17 @@ pub type Result<T> = StdResult<T, Error>;
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0.kind {
-            Kind::Builder(err) => write!(f, "Error building client: {}", err.to_string()),
-            Kind::Network(err) => write!(f, "Error sending request: {}", err.to_string()),
+        match self.kind {
+            Kind::Builder(err) => {
+                write!(f, "Error building client: ")?;
+                err.fmt(f)
+            }
+            Kind::Network(err) => {
+                write!(f, "Error sending request: ")?;
+                err.fmt(f)
+            }
             Kind::CSRF(x) => {
-                write!(f, "Error getting csrf token: ");
+                write!(f, "Error getting csrf token: ")?;
                 x.fmt(f)
             }
             Kind::API => {
@@ -36,10 +40,13 @@ impl fmt::Display for Error {
                 self.write_description(f)
             }
             Kind::Regex => {
-                write!(f, "Regex not matched");
+                write!(f, "Regex not matched")?;
                 self.write_description(f)
             }
-            Kind::Email(err) => write!(f, "Email cliend: {}", err.to_string()),
+            Kind::Email(err) => {
+                write!(f, "Email client: ")?;
+                err.fmt(f)
+            }
             Kind::TestCount(count, expect) => {
                 write!(f, "Test count not match. Expected {} got {}", expect, count)
             }
@@ -48,7 +55,7 @@ impl fmt::Display for Error {
 }
 impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match self.0.kind {
+        match self.kind {
             Kind::Builder(x) | Kind::Network(x) => Some(&x),
             Kind::CSRF(x) => Some(x.as_ref()),
             Kind::Email(e) => Some(&e),
@@ -57,23 +64,20 @@ impl StdError for Error {
     }
 }
 impl Error {
-    fn new(inner: Inner) -> Self {
-        Self(Box::new(inner))
-    }
     pub(super) fn with_kind(kind: Kind) -> Self {
-        Self::new(Inner {
+        Self {
             kind,
             description: None,
-        })
+        }
     }
     pub(super) fn with_description<T: Into<String>>(kind: Kind, description: T) {
-        Self::new(Inner {
+        Self {
             kind,
             description: Some(T::into(description)),
-        })
+        }
     }
     fn write_description(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if let Some(d) = self.0.description {
+        if let Some(d) = self.description {
             write!(f, ": {}", d)
         } else {
             Ok(())
@@ -85,8 +89,8 @@ pub(super) fn network_error(err: reqwest::Error) -> Error {
     Error::with_kind(Kind::Network(err))
 }
 pub(super) fn regex_mismatch(description: Option<String>) -> Error {
-    Error::new(Inner {
+    Error {
         kind: Kind::Regex,
         description,
-    })
+    }
 }
